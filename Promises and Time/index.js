abd9f1342e588b82b96daf7f99f3177c0e2b604c
JaydipJadhav25 <jaydipdjadhav@gmail.com>
2026-07-19 23:34:45 +0530

    debounce

--- Promises and Time/index.js
+++ Promises and Time/index.js
@@ -1,49 +1,73 @@
-const dmeo = new Promise((resolver , reject)=>{
-    let success = true;
+// const dmeo = new Promise((resolver , reject)=>{
+//     let success = true;
 
-    if(success){
-        resolver(2);
-    }else{
-        reject("this is rejector");
-    }
-
-
-
-    // setTimeout(()=>{
-
-    //     resolver("done......!");
-
-    // },5000)
-});
+//     if(success){
+//         resolver(2);
+//     }else{
+//         reject("this is rejector");
+//     }
 
 
 
-// console.log(dmeo);
+//     // setTimeout(()=>{
 
-// dmeo
-// .then((res)=>{
+//     //     resolver("done......!");
 
-//  console.log(res);
+//     // },5000)
+// });
+
+
+
+// // console.log(dmeo);
+
+// // dmeo
+// // .then((res)=>{
+
+// //  console.log(res);
  
-// })
-// .catch((err)=>{
+// // })
+// // .catch((err)=>{
 
-//  console.log(err);
+// //  console.log(err);
 
-// })
-
-
-const val = await dmeo;
-console.log(val);
+// // })
 
 
+// const val = await dmeo;
+// console.log(val);
 
-//setintervel
-const id =  setInterval(()=>{
- console.log("hii");
-},2000)
 
-clearInterval(id);
+
+// //setintervel
+// const id =  setInterval(()=>{
+//  console.log("hii");
+// },2000)
+
+// clearInterval(id);
+
+
+
+const startbtn = document.getElementById("start");
+const stopbtn = document.getElementById("stop");
+const p = document.getElementById("para");
+
+let timer = 0;
+
+let num = 0;
+startbtn.addEventListener('click' , ()=>{
+    timer = setInterval(()=>{
+        p.innerText = num;
+        num++;
+        console.log(num);
+    },500);
+})
+
+
+stopbtn.addEventListener('click' , ()=>{
+    clearInterval(timer);
+})
+
+
 
 
 
