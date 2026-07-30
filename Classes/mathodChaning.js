3cd409fc735d14b362056f096980bd92676240a3
JaydipJadhav25 <jaydipdjadhav@gmail.com>
2026-07-30 23:25:00 +0530

    method chaining

--- Classes/mathodChaning.js
+++ Classes/mathodChaning.js
@@ -5,3 +5,54 @@
 // Without return this
 
 
+class Calculator{
+
+    constructor(value){
+        this.result = value;
+    }
+
+
+    //addition
+    add(number){
+        this.result +=number;
+        return this;
+    }
+
+    sub(number){
+        this.result -=number;
+        return this;
+
+    }
+
+    div(number){
+        this.result /=number;
+        return this;
+
+    }
+
+    multi(number){
+        this.result *=number;
+        return this;
+
+    }
+
+
+
+    printResult(){
+        console.log("Result : " , this.result);
+    }
+
+
+
+}
+
+
+
+const c = new Calculator(0);
+
+//  const ref =  c.add(30)
+// //  console.log(ref)
+
+c.add(20).sub(30).multi(30).printResult()
+
+
